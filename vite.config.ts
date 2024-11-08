c47a9ddc14c092edbb8040108713f657db084385
Jordan Fielding <jordo2410@gmail.com>
2024-11-08 10:22:34 +1000

    Changing to working vue Site

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    watch:{
-      usePolling:true
-    }
-  },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
